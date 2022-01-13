3196c372f6e93693c617df5a95b7603126901227
libaiqi-gt <892208148@qq.com>
2022-01-13 11:09:26 +0800

    暂存

--- src/components/MainSider/index.js
+++ src/components/MainSider/index.js
@@ -1,17 +1,32 @@
 import { Menu } from 'antd'
-import { UserOutlined } from '@ant-design/icons'
 import React from 'react'
-// import LogoImage from '@/asset/image/logo.png'
+import { useNavigate } from 'react-router-dom'
+import { routeConfig } from '@/router'
+import LogoImage from '@/asset/image/logo.png'
 import './index.scss'
 
 export default function MainSider() {
+  const handleClick = item => {
+    // useNavigate(item.key);
+    console.log(item, '点击');
+  }
+
+  const renderMenu = (menus) => 
+    menus.map(menu => 
+      menu.menuType === 'SUBMENU' ? (
+          <Menu.SubMenu key={menu.fullPath} title={menu.name} icon={menu.icon}>{renderMenu(menu?.children ?? [])}</Menu.SubMenu>
+      ) : menu.menuType === 'MENU' ? (
+          <Menu.Item key={menu.fullPath} icon={menu.icon}>{menu.name}</Menu.Item>
+      ) : null
+    )
+  
   return (
     <div data-component="MainSider">
       <div className='logo'>
-        {/* <img src={LogoImage} /> */}
+        <img src={LogoImage} />
       </div>
-      <Menu theme='dark' mode='inline'>
-        <Menu.Item key={1} icon={<UserOutlined/>}>nav 1</Menu.Item>
+      <Menu theme='dark' mode='inline' onClick={handleClick}>
+        {renderMenu(routeConfig)}
       </Menu>
     </div>
   )
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import * as AntdIcons from '@ant-design/icons'
+import { createElement } from 'react'
+import WelCome from "@/page/WelCome";
+import Home from "@/page/Home";
+import UserInfo from '../page/Home/UserInfo';
+
+export const routeConfig = [
+  {
+    path: '/',
+    fullPath: '/page/WelCome',
+    component: <WelCome/>,
+    name: '简介',
+    menuType: 'PAGE'
+  },
+  {
+    path: '/Home',
+    fullPath: '/page/Home',
+    component: <Home/>,
+    name: '我的',
+    menuType: 'SUBMENU',
+    icon: createElement(AntdIcons['UserOutlined']),
+    children: [
+      {
+        path: '/Home/UserInfo',
+        fullPath: '/page/Home/UserInfo',
+        component: <UserInfo/>,
+        name: '个人信息',
+        menuType: 'MENU',
+        icon: createElement(AntdIcons['BarsOutlined'])
+      }
+    ]
+  }
+]
